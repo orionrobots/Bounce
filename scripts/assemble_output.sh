c6f24522ebaa687d42f5714eb897139983c8499f
Danny Staple <danny@orionrobots.co.uk>
2016-04-10 12:12:29 +0100

    Woops. That was always supposed to be exported.

--- scripts/assemble_output.sh
+++ scripts/assemble_output.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -eu -o pipefail
-set OUTPUT_DIR=output
-set SRC_DIR=Bounce
-set SRC_LIST=jquery* glyphic* Examples* bounce_window.html bounce.css bounce*.png background.js \
+export OUTPUT_DIR=output
+export SRC_DIR=Bounce
+export SRC_LIST=jquery* glyphic* Examples* bounce_window.html bounce.css bounce*.png background.js \
     blockly-nodemcu/blocks_compressed.js blockly-nodemcu/msg/messages.js CustomNodeBlocks.js
 
 # TODO: Combine CSS.
-set LIBRARY_SRC_LIST=\
+export LIBRARY_SRC_LIST=\
     closure-library/closure/goog/css/common.css closure-library/closure/goog/css/menu.css \
     closure-library/closure/goog/css/menuitem.css closure-library/closure/goog/css/menuseparator.css \
     closure-library/closure/goog/css/toolbar.css
