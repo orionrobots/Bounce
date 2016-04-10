fe961676ad9894083808afef48bfbd54cd3c790c
Danny Staple <danny@orionrobots.co.uk>
2016-04-10 12:20:34 +0100

    Perhaps need to quote those. LS to see what I ended up with there.

--- scripts/assemble_output.sh
+++ scripts/assemble_output.sh
@@ -2,14 +2,14 @@
 set -eu -o pipefail
 export OUTPUT_DIR=output
 export SRC_DIR=Bounce
-export SRC_LIST=jquery* glyphic* Examples* bounce_window.html bounce.css bounce*.png background.js \
-    blockly-nodemcu/blocks_compressed.js blockly-nodemcu/msg/messages.js CustomNodeBlocks.js
+export SRC_LIST="jquery* glyphic* Examples* bounce_window.html bounce.css bounce*.png background.js \
+    blockly-nodemcu/blocks_compressed.js blockly-nodemcu/msg/messages.js CustomNodeBlocks.js"
 
 # TODO: Combine CSS.
-export LIBRARY_SRC_LIST=\
+export LIBRARY_SRC_LIST="\
     closure-library/closure/goog/css/common.css closure-library/closure/goog/css/menu.css \
     closure-library/closure/goog/css/menuitem.css closure-library/closure/goog/css/menuseparator.css \
-    closure-library/closure/goog/css/toolbar.css
+    closure-library/closure/goog/css/toolbar.css"
 
 for file in ${SRC_LIST}; do
     cp ${SRC_DIR}/${file} ${OUTPUT_DIR}
