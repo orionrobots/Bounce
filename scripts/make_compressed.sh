603815b0086dc37dd8668886900bc356f3aca9b3
Danny Staple <danny@orionrobots.co.uk>
2016-04-10 12:08:21 +0100

    Fix the unzip. Put them in the scripts folder. Make them executable.

--- scripts/make_compressed.sh
+++ scripts/make_compressed.sh
@@ -3,7 +3,7 @@ set -e
 closure-library\closure\bin\build\closurebuilder.py \
     --input=app\start.js \
     -o compiled \
-    -c compiler-latest/compiler.jar \
+    -c compiler/compiler.jar \
     --output_file=output/start_compressed.js \
     --root=Bounce/app \
     --root=closure-library \
