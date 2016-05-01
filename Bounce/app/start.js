569eaef3661c110b0a38b8072ea6745ce0af3148
Danny Staple <danny@orionrobots.co.uk>
2016-05-01 12:48:57 +0100

    Add start of device files stuff.

--- Bounce/app/start.js
+++ Bounce/app/start.js
@@ -289,4 +289,9 @@ $(function () {
     mcu_console = new OutputConsole($('#output'));
     ui = new BounceUI();
     workspace.addChangeListener(ui.changed);
+
+    var files_on_chip_toolbar = new goog.ui.Toolbar();
+    files_on_chip_toolbar.decorate(goog.dom.getElement('files_on_chip_toolbar'));
+
 });
+
