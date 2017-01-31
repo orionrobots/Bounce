2452b3be46a5e33670c6d8dca7a15b6569d1f563
Danny Staple <danny@orionrobots.co.uk>
2017-01-31 09:01:13 +0000

    Starting to shape like bounce again.

--- main.js
+++ main.js
@@ -7,8 +7,7 @@ let win;
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
-
+  win = new BrowserWindow({width: 800, height: 600});
   // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
