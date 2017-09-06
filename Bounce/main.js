cbc484c16434930b61498eb64f7d483deb6b7a23
Danny Staple <danny@orionrobots.co.uk>
2017-09-06 21:22:28 +0100

    Load the icon.

--- Bounce/main.js
+++ Bounce/main.js
@@ -7,7 +7,7 @@ let win;
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 1024, height: 600});
+  win = new BrowserWindow({width: 1024, height: 600, icon: 'bounce-16.png'});
   // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'bounce_window.html'),
