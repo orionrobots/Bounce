eb49171ecd5630fd41cbb7cbdf629784db7872bb
Danny Staple <danny@orionrobots.co.uk>
2017-09-07 19:09:32 +0100

    Start maximised.

--- Bounce/main.js
+++ Bounce/main.js
@@ -7,7 +7,8 @@ let win;
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 1024, height: 600, icon: 'bounce-16.png'});
+  win = new BrowserWindow({icon: 'bounce-16.png'});
+  win.maximize();
   // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'bounce_window.html'),
